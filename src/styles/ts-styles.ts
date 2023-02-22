3aad3ed56167e329ab7f5479bf4cfa13834af22d
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2023-02-22 11:24:06 +0000

    add smart-pokemon-go init

---
+++ src/styles/ts-styles.ts
@@ -0,0 +1,23 @@
+import { Work_Sans, Nothing_You_Could_Do, Cabin } from '@next/font/google'
+
+export const ffHeaders = Work_Sans({ 
+  subsets: ['latin'],
+  weight: ['400', '700', '800']
+})
+
+export const ffSpecial = Nothing_You_Could_Do({ 
+  subsets: ['latin'],
+  weight: '400'
+})
+
+export const ffBody = Cabin({ 
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
+
+/* WAY TO USE LOCAL FONTS
+import localFont from '@next/font/local'
+const ffLocal = localFont({
+  src: '/fonts/Inter-Regular.woff2',
+})
+ */
