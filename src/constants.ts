3b3430e5c49da5a219cfd64b56844a37d3aae163
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2023-02-28 08:47:05 +0000

    type icons and constant object done

---
+++ src/constants.ts
@@ -0,0 +1,79 @@
+export const pokemon_types : {
+  [index: string] : {
+    image: string
+    type: string
+  }
+}= {
+  "Bug": {
+    image: "/Pokemon_Type_Icon_Bug.svg",
+    type: "Bicho"
+  },
+  "Dark": {
+    image: "/Okemon_Type_Icon_Dark.svg",
+    type: "Siniestro"
+  },
+  "Dragon": {
+    image: "/Pokemon_Type_Icon_Dragon.svg",
+    type: "Dragón"
+  },
+  "Electric": {
+    image: "/Pokemon_Type_Icon_Electric.svg",
+    type: "Eléctrico"
+  },
+  "Fairy": {
+    image: "/Pokemon_Type_Icon_Fairy.svg",
+    type: "Hada"
+  },
+  "Fighting": {
+    image: "/Pokemon_Type_Icon_Fighting.svg",
+    type: "Lucha"
+  },
+  "Fire": {
+    image: "/Pokemon_Type_Icon_Fire.svg",
+    type: "Fuego"
+  },
+  "Flying": {
+    image: "/Pokemon_Type_Icon_Flying.svg",
+    type: "Volador"
+  },
+  "Ghost": {
+    image: "/Pokemon_Type_Icon_Ghost.svg",
+    type: "Fantasma"
+  },
+  "Grass": {
+    image: "/Pokemon_Type_Icon_Grass.svg",
+    type: "Planta"
+  },
+  "Ground": {
+    image: "/Pokemon_Type_Icon_Ground.svg",
+    type: "Tierra"
+  },
+  "Ice": {
+    image: "/Pokemon_Type_Icon_Ice.svg",
+    type: "Hielo"
+  },
+  "Normal": {
+    image: "/Pokemon_Type_Icon_Normal.svg",
+    type: "Normal"
+  },
+  "Poison": {
+    image: "/Pokemon_Type_Icon_Poison.svg",
+    type: "Veneno"
+  },
+  "Psychic": {
+    image: "/Pokemon_Type_Icon_Psychic.svg",
+    type: "Psíquico"
+  },
+  "Rock": {
+    image: "/Pokemon_Type_Icon_Rock.svg",
+    type: "Roca"
+  },
+  "Steel": {
+    image: "/Pokemon_Type_Icon_Steel.svg",
+    type: "Acero"
+  },
+  "Water": {
+    image: "/Pokemon_Type_Icon_Water.svg",
+    type: "Agua"
+  }
+}
